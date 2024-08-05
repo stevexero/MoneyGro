f4d2180d3728da87c47838d4ece1e94a39a772fb
stevexero <steveanthony999@gmail.com>
2024-08-05 11:22:48 -0700

    Created NAVBAR component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
-import { GrMoney, GrClose, GrAdd } from 'react-icons/gr';
+import { GrClose, GrAdd } from 'react-icons/gr';
 import { FaPercent } from 'react-icons/fa6';
 import { TbLockDollar } from 'react-icons/tb';
 import { CgRename } from 'react-icons/cg';
 import { FaTimes } from 'react-icons/fa';
+import Navbar from './components/Navbar';
 
 function App() {
   const [inputs, setInputs] = useState({
@@ -231,7 +232,8 @@ function App() {
   return (
     <div className='container mx-auto px-4 md:px-0 overflow-x-hidden'>
       {/* NAV */}
-      <nav className='navbar rounded-full'>
+      <Navbar />
+      {/* <nav className='navbar rounded-full'>
         <div className='flex-none'>
           <GrMoney />
         </div>
@@ -240,13 +242,11 @@ function App() {
         </div>
         <div className='flex-none'>
           <label className='swap swap-rotate'>
-            {/* this hidden checkbox controls the state */}
             <input
               type='checkbox'
               className='theme-controller'
               value='synthwave'
             />
-            {/* sun icon */}
             <svg
               className='swap-off h-6 w-6 fill-current'
               xmlns='http://www.w3.org/2000/svg'
@@ -254,7 +254,6 @@ function App() {
             >
               <path d='M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z' />
             </svg>
-            {/* moon icon */}
             <svg
               className='swap-on h-6 w-6 fill-current'
               xmlns='http://www.w3.org/2000/svg'
@@ -264,7 +263,7 @@ function App() {
             </svg>
           </label>
         </div>
-      </nav>
+      </nav> */}
 
       <div className='container flex flex-col items-center mt-12'>
         <label className='relative block w-full max-w-lg'>
